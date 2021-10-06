9466cc181c1a9e84ae051bb92ec57f0045791e8a
Jasa-Hollow <74333561+Jasa-Hollow@users.noreply.github.com>
2021-10-06 06:40:47 -0600

    KitCo - iOS

---
+++ KitCo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Restaurant_recipes.Services;
+using Restaurant_recipes.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Restaurant_recipes
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ KitCo/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Restaurant_recipes.ViewModels;
+using Restaurant_recipes.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Restaurant_recipes
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
