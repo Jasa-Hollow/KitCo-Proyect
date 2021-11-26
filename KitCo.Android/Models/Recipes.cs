aea4a46ff51e7156817a2c70ce25f198a4aefec7
Jasa-Hollow <74333561+Jasa-Hollow@users.noreply.github.com>
2021-11-26 11:07:43 -0600

    Update #1 - Views, ViewModels and Models
    Adding new views and models in the app.

---
+++ KitCo.Android/Models/Recipes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitCoApp.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+
+    }
+}
